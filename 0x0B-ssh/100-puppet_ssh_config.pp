118a903b06efecc13452d2bafa2ac72798780ba3
Haile Gebresilassie <hailye2013@gmail.com>
2022-09-19 22:43:59 +0300

    My Completed task 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,8 @@
-# Changes SSH config file
-exec { 'echo':
-  path    => 'usr/bin:/bin',
-  command => 'echo "    IdentityFile ~/.ssh/holberton\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
-  returns => [0,1],
+# sets up a client SSH configuration file so that we can connect to a server without using a password
+file_line { 'Declare identity file':
+include stdlib
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
+  replace => true,
 }
+
